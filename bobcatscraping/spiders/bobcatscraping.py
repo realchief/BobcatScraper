270a88f8ead60487f39c08fd8928d4c1621d9bca
realchief <stryokka@gmail.com>
2018-12-18 02:43:26 -0500

    updated bobcat scraping

--- bobcatscraping/spiders/bobcatscraping.py
+++ bobcatscraping/spiders/bobcatscraping.py
@@ -10,10 +10,8 @@ import json
 
 
 class SiteProductItem(Item):
-    product_name = Field()
-    images = Field()
-    feature = Field()
-    specification = Field()
+    product_info = Field()
+    part_info = Field()
 
 
 class BobcatScraper (scrapy.Spider):
@@ -34,68 +32,94 @@ class BobcatScraper (scrapy.Spider):
 
     def parse_page(self, response):
 
-        page_links = response.xpath('//a[@class="frameImg"]/@href').extract()
+        product_info = []
+        product_group_list = response.xpath('//a[contains(@class, "product-item")]')
+        for product_group in product_group_list:
 
-        for p_link in page_links:
-            if 'https' in p_link:
-                sub_link = p_link
-            else:
-                sub_link = self.DOMAIN_URL + p_link
-            yield Request(url=sub_link, callback=self.parse_product, dont_filter=True, headers=self.headers)
+            product_name = None
+            try:
+                product_name = product_group.xpath('.//h5/text()')[0].extract()
+            except:
+                pass
 
-    def parse_product(self, response):
-        product = SiteProductItem()
+            product_image = None
+            try:
+                product_image = product_group.xpath('.//@data-src')[0].extract()
+            except:
+                pass
 
-        product_name = self._parse_name(response)
-        product['product_name'] = product_name
+            product_feature = None
+            try:
+                product_feature = product_group.xpath('.//p/text()')[0].extract()
+            except:
+                pass
 
-        images = self._parse_images(response)
-        product['images'] = images
+            product_info.append({'product_name': product_name,
+                                 'product_image': product_image,
+                                 'product_feature': product_feature
+                                 })
 
-        feature = self._parse_feature(response)
-        product['feature'] = feature
+        bobcat = SiteProductItem()
+        bobcat['product_info'] = product_info
 
-        specification = self._parse_specification(response)
-        product['specification'] = specification
+        # meta = response.meta
+        # meta['bobcat'] = bobcat
+        return bobcat
 
-        yield product
-
-    @staticmethod
-    def _parse_name(response):
-        title = response.xpath('//title/text()').extract()
-        return title[0].strip() if title else None
-
-    def _parse_images(self, response):
-        images = []
-        asset_images = response.xpath('//div[@class="frameImg"]//img/@data-lazy').extract()
-        if asset_images:
-            for image in asset_images:
-                image = self.DOMAIN_URL + image
-                images.append(image)
-        return images
-
-    @staticmethod
-    def _parse_feature(response):
-        features = response.xpath('//div[contains(@id,"Features")]//a[contains(@class, "btn-gold")]//text()').extract()
-        return features
-
-    def _parse_specification(self, response):
-        result = []
-        t_header = response.xpath('//table[contains(@class, "table-striped")]/thead/tr/th/text()').extract()
-        t_body = response.xpath('//table[contains(@class, "table-striped")]/div[contains(@class, "tablePar")]//tr').extract()
-        if len(t_header) > 0:
-            for body in t_body:
-                spec = {}
-                tree_body = html.fromstring(body)
-                spec_body_vals = tree_body.xpath('//tr/td/text()')
-
-                for i in range(0, len(t_header) - 1):
-                    spec_header = self._clean_text(t_header[i])
-                    if spec_body_vals:
-                        spec_body = self._clean_text(spec_body_vals[i])
-                        spec[spec_header] = spec_body
-                result.append(spec)
-        return result
+    #
+    # def parse_product(self, response):
+    #     product = SiteProductItem()
+    #
+    #     product_name = self._parse_name(response)
+    #     product['product_name'] = product_name
+    #
+    #     images = self._parse_images(response)
+    #     product['images'] = images
+    #
+    #     feature = self._parse_feature(response)
+    #     product['feature'] = feature
+    #
+    #     specification = self._parse_specification(response)
+    #     product['specification'] = specification
+    #
+    #     yield product
+    #
+    # @staticmethod
+    # def _parse_name(response):
+    #     title = response.xpath('//title/text()').extract()
+    #     return title[0].strip() if title else None
+    #
+    # def _parse_images(self, response):
+    #     images = []
+    #     asset_images = response.xpath('//div[@class="frameImg"]//img/@data-lazy').extract()
+    #     if asset_images:
+    #         for image in asset_images:
+    #             image = self.DOMAIN_URL + image
+    #             images.append(image)
+    #     return images
+    #
+    # @staticmethod
+    # def _parse_feature(response):
+    #     features = response.xpath('//div[contains(@id,"Features")]//a[contains(@class, "btn-gold")]//text()').extract()
+    #     return features
+    #
+    # def _parse_specification(self, response):
+    #     result = []
+    #     t_header = response.xpath('//table[contains(@class, "table-striped")]/thead/tr/th/text()').extract()
+    #     t_body = response.xpath('//table[contains(@class, "table-striped")]/div[contains(@class, "tablePar")]//tr').extract()
+    #     if len(t_header) > 0:
+    #         for body in t_body:
+    #             spec = {}
+    #             tree_body = html.fromstring(body)
+    #             spec_body_vals = tree_body.xpath('//tr/td/text()')
+    #
+    #             for i in range(0, len(t_header) - 1):
+    #                 spec_header = self._clean_text(t_header[i])
+    #                 if spec_body_vals:
+    #                     spec_body = self._clean_text(spec_body_vals[i])
+    #                     spec[spec_header] = spec_body
+    #             result.append(spec)
+    #     return result
 
 
     @staticmethod
